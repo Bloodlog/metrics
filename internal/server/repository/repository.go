d7db9094b52507a4460bb07d4fc19e070c1e7c85
Maksim Fomin <bloodlog@gmail.com>
2024-12-10 09:34:29 +0400

    iter5 Вернул блокировку чтения и добавил глубокое копирование мап

--- internal/server/repository/repository.go
+++ internal/server/repository/repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"errors"
+	"sync"
 )
 
 type MetricType string
@@ -11,25 +12,33 @@ type MetricStorage interface {
 	GetGauge(name string) (float64, error)
 	SetCounter(name string, value uint64)
 	GetCounter(name string) (uint64, error)
+	Gauges() map[string]float64
+	Counters() map[string]uint64
 }
 
 type MemStorage struct {
 	gauges   map[string]float64
 	counters map[string]uint64
+	mu       *sync.RWMutex
 }
 
 func NewMemStorage() *MemStorage {
 	return &MemStorage{
+		mu:       &sync.RWMutex{},
 		gauges:   make(map[string]float64),
 		counters: make(map[string]uint64),
 	}
 }
 
 func (ms *MemStorage) SetGauge(name string, value float64) {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
 	ms.gauges[name] = value
 }
 
 func (ms *MemStorage) GetGauge(name string) (float64, error) {
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
 	value, exists := ms.gauges[name]
 	if !exists {
 		return 0, errors.New("gauge metric not found")
@@ -38,10 +47,14 @@ func (ms *MemStorage) GetGauge(name string) (float64, error) {
 }
 
 func (ms *MemStorage) SetCounter(name string, value uint64) {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
 	ms.counters[name] += value
 }
 
 func (ms *MemStorage) GetCounter(name string) (uint64, error) {
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
 	value, exists := ms.counters[name]
 	if !exists {
 		return 0, errors.New("counter metric not found")
@@ -50,9 +63,21 @@ func (ms *MemStorage) GetCounter(name string) (uint64, error) {
 }
 
 func (ms *MemStorage) Gauges() map[string]float64 {
-	return ms.gauges
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
+	result := make(map[string]float64, len(ms.gauges))
+	for k, v := range ms.gauges {
+		result[k] = v
+	}
+	return result
 }
 
 func (ms *MemStorage) Counters() map[string]uint64 {
-	return ms.counters
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
+	result := make(map[string]uint64, len(ms.counters))
+	for k, v := range ms.counters {
+		result[k] = v
+	}
+	return result
 }
