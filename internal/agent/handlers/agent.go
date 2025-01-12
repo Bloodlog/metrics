857d3df806e84585d3d1d5fe5a2f0a466bbaf7a8
Maksim Fomin <bloodlog@gmail.com>
2025-01-12 12:30:42 +0400

    iter12 Добавил флаг batch для агента

--- internal/agent/handlers/agent.go
+++ internal/agent/handlers/agent.go
@@ -56,31 +56,76 @@ func (h *Handlers) Handle() error {
 			counter++
 
 		case <-reportTicker.C:
-			metricsRequests := service.MetricsUpdateRequests{}
 			delta := int64(counter)
-
-			metric := service.MetricsUpdateRequest{
-				Delta: &delta,
-				ID:    nameCounter,
-				MType: typeCounter,
+			var err error
+			if h.configs.Batch {
+				err = h.sendBatch(metrics, delta)
+			} else {
+				err = h.sendApi(metrics, delta)
 			}
-			metricsRequests.Metrics = append(metricsRequests.Metrics, metric)
-
-			for _, metric := range metrics {
-				valueFloat := float64(metric.Value)
-				metric := service.MetricsUpdateRequest{
-					Value: &valueFloat,
-					ID:    metric.Name,
-					MType: typeMetricName,
-				}
-				metricsRequests.Metrics = append(metricsRequests.Metrics, metric)
-			}
-
-			err := service.SendMetricsBatch(h.client, metricsRequests)
 			if err != nil {
 				return fmt.Errorf("failed to send metric to server: %w", err)
 			}
 			counter = 0
 		}
 	}
+}
+
+func (h *Handlers) sendBatch(metrics []repository.Metric, counter int64) error {
+	metricsRequests := service.MetricsUpdateRequests{}
+	metric := service.MetricsUpdateRequest{
+		Delta: &counter,
+		ID:    nameCounter,
+		MType: typeCounter,
+	}
+	metricsRequests.Metrics = append(metricsRequests.Metrics, metric)
+
+	for _, metric := range metrics {
+		valueFloat := float64(metric.Value)
+		metric := service.MetricsUpdateRequest{
+			Value: &valueFloat,
+			ID:    metric.Name,
+			MType: typeMetricName,
+		}
+		metricsRequests.Metrics = append(metricsRequests.Metrics, metric)
+	}
+
+	err := service.SendMetricsBatch(h.client, metricsRequests)
+	if err != nil {
+		return fmt.Errorf("failed to send metric to server: %w", err)
+	}
+
+	return nil
+}
+
+func (h *Handlers) sendApi(metrics []repository.Metric, counter int64) error {
+	metricCounterRequest := service.MetricsCounterRequest{
+		Delta: &counter,
+		ID:    nameCounter,
+		MType: typeCounter,
+	}
+
+	err := service.SendIncrement(h.client, metricCounterRequest)
+
+	counter = 0
+	if err != nil {
+		return fmt.Errorf("failed to send Increment %d to server: %w", counter, err)
+	}
+
+	for _, metric := range metrics {
+		valueFloat := float64(metric.Value)
+
+		MetricGaugeUpdateRequest := service.MetricsGaugeUpdateRequest{
+			Value: &valueFloat,
+			ID:    metric.Name,
+			MType: typeMetricName,
+		}
+
+		err := service.SendMetric(h.client, MetricGaugeUpdateRequest)
+		if err != nil {
+			return fmt.Errorf("failed to send metric %s to server: %w", metric.Name, err)
+		}
+	}
+
+	return nil
 }
