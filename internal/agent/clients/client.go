cd76dd1a052bcf9572637be13473192b29de9e94
Maksim Fomin <bloodlog@gmail.com>
2025-01-21 22:07:57 +0400

    iter13 Исправил client для retry запросов

--- internal/agent/clients/client.go
+++ internal/agent/clients/client.go
@@ -1,7 +1,6 @@
 package clients
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
@@ -11,41 +10,20 @@ import (
 )
 
 func CreateClient(serverAddr string, logger *zap.SugaredLogger) *resty.Client {
-	handlerLogger := logger.With("client", "send request")
 	retryClient := retryablehttp.NewClient()
 	retryClient.RetryMax = 3
-	retryClient.Backoff = CustomBackoff
+	retryClient.Backoff = customBackoff
 
-	client := resty.New().
+	restyClient := resty.New().
 		SetBaseURL(serverAddr).
 		SetHeader("Content-Encoding", "gzip").
 		SetHeader("Content-Type", "application/json")
+	restyClient.SetTransport(retryClient.StandardClient().Transport)
 
-	clientRetry := client.
-		OnBeforeRequest(func(client *resty.Client, req *resty.Request) error {
-			handlerLogger.Info("retry request")
-			httpReq, err := retryablehttp.NewRequest(req.Method, req.URL, req.Body)
-			if err != nil {
-				return fmt.Errorf("failed to create retryable request: %w", err)
-			}
-
-			resp, err := retryClient.Do(httpReq)
-			if err != nil {
-				return fmt.Errorf("failed to send request with retries: %w", err)
-			}
-			defer func() {
-				if err := resp.Body.Close(); err != nil {
-					handlerLogger.Infoln("failed to close response body: %v", err)
-				}
-			}()
-
-			return nil
-		})
-
-	return clientRetry
+	return restyClient
 }
 
-func CustomBackoff(min, max time.Duration, attemptNum int, resp *http.Response) time.Duration {
+func customBackoff(min, max time.Duration, attemptNum int, resp *http.Response) time.Duration {
 	backoffIntervals := []time.Duration{1 * time.Second, 3 * time.Second, 5 * time.Second}
 	if attemptNum-1 < len(backoffIntervals) {
 		return backoffIntervals[attemptNum-1]
