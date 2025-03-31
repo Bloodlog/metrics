2b39673a8bb71674fefe96a862d32f0bc737d7c1
Maksim Fomin <bloodlog@gmail.com>
2025-03-31 23:34:53 +0400

    Покрытие кода 42.2%

---
+++ internal/server/handlers/api/batch_update_test.go
@@ -0,0 +1,74 @@
+package api
+
+import (
+	"metrics/internal/server/service"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"go.uber.org/zap"
+
+	"github.com/go-chi/chi/v5"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
+
+	"metrics/internal/server/repository"
+)
+
+func TestBatchUpdateHandler(t *testing.T) {
+	testCases := []struct {
+		name         string
+		requestBody  string
+		expectedCode int
+	}{
+		{
+			name: "Success",
+			requestBody: `[
+				{"id": "PollCount", "type": "counter", "delta": 100},
+				{"id": "Allocate", "type": "gauge", "value": 25.5}
+			]`,
+			expectedCode: http.StatusOK,
+		},
+		{
+			name:         "Invalid JSON",
+			requestBody:  `{"id": "PollCount", "type": "counter", "delta": "not_a_number"}`,
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:         "Missing Required Fields",
+			requestBody:  `{"id": "PollCount"}`,
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:         "Unsupported Metric Type",
+			requestBody:  `{"id": "Unknown", "type": "unknown"}`,
+			expectedCode: http.StatusBadRequest,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			logger := zap.NewNop()
+			sugar := logger.Sugar()
+
+			memStorage, _ := repository.NewMemStorage()
+			r := chi.NewRouter()
+			metricService := service.NewMetricService(memStorage, sugar)
+			apiHandler := NewHandler(metricService, sugar)
+			r.Post("/updates", apiHandler.UpdatesHandler())
+
+			srv := httptest.NewServer(r)
+			defer srv.Close()
+
+			resp, err := resty.New().R().
+				SetHeader("Content-Type", "application/json").
+				SetBody(tc.requestBody).
+				Post(srv.URL + "/updates")
+
+			assert.NoError(t, err, "Error making HTTP request")
+
+			assert.Equal(t, tc.expectedCode, resp.StatusCode(), "Unexpected status code")
+		})
+	}
+}
