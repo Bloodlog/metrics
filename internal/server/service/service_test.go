5646537c42f7d08fa170a46511a787e597406e20
Maksim Fomin <bloodlog@gmail.com>
2025-03-31 23:43:43 +0400

    Покрытие кода 43.3%

--- internal/server/service/service_test.go
+++ internal/server/service/service_test.go
@@ -211,3 +211,61 @@ func TestGetMetrics(t *testing.T) {
 		assert.Equal(t, resp.Gauges[gaugeID], gaugeValue)
 	})
 }
+
+func TestUpdateMultiple(t *testing.T) {
+	ctx := context.Background()
+	memStorage, _ := repository.NewMemStorage()
+	logger := zap.NewNop()
+	sugar := logger.Sugar()
+	metricService := NewMetricService(memStorage, sugar)
+
+	counterID := "testCounter"
+	gaugeID := "testGauge"
+
+	_, _ = memStorage.SetCounter(ctx, counterID, 42)
+	_, _ = memStorage.SetGauge(ctx, gaugeID, 123.45)
+
+	t.Run("Update multiple metrics", func(t *testing.T) {
+		metrics := []dto.MetricsUpdateRequest{
+			{
+				ID:    counterID,
+				MType: "counter",
+				Delta: new(int64),
+			},
+			{
+				ID:    gaugeID,
+				MType: "gauge",
+				Value: new(float64),
+			},
+		}
+		*metrics[0].Delta = 10
+		*metrics[1].Value = 150.5
+
+		err := metricService.UpdateMultiple(ctx, metrics)
+		assert.NoError(t, err)
+
+		updatedCounter, _ := memStorage.GetCounter(ctx, counterID)
+		updatedGauge, _ := memStorage.GetGauge(ctx, gaugeID)
+
+		assert.Equal(t, uint64(52), updatedCounter)
+		assert.Equal(t, 150.5, updatedGauge)
+	})
+
+	t.Run("Update with nil values", func(t *testing.T) {
+		metrics := []dto.MetricsUpdateRequest{
+			{
+				ID:    counterID,
+				MType: "counter",
+				Delta: nil,
+			},
+			{
+				ID:    gaugeID,
+				MType: "gauge",
+				Value: nil,
+			},
+		}
+
+		err := metricService.UpdateMultiple(ctx, metrics)
+		assert.NoError(t, err)
+	})
+}
